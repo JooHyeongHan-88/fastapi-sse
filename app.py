7c8c95c0dd3262a1ef9d6cf762720edeaf0792bb
JooHyeong Han <hanjoo8821@postech.ac.kr>
2023-12-17 14:43:36 +0900

    websocket 방식 추가

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import asyncio
+import websockets
 import streamlit as st
 import json
 import pandas as pd
-
 import requests
 
 
 URL = "http://127.0.0.1:8000"
+URL_WS = "ws://127.0.0.1:8000"
 
 
 def start_process():
@@ -20,13 +22,35 @@ def get_result():
     res = pd.DataFrame(res)
     return res
 
+async def start_process_ws():
+    async with websockets.connect(URL_WS + "/process") as websocket:
+        processing = True
+        while processing:
+            res = await websocket.recv()
+            res = eval(json.loads(res))
+            if 'status' in res:
+                pbar.progress(res['status'], res['msg'])
+            else:
+                processing = False
+                pbar.success("Complete")
+                res = json.loads(res["result"])
+                res = pd.DataFrame(res)
+                st.write(res)
+
 
 pbar = st.empty()
 
 
-if st.button("실행"):
+c1, c2 = st.columns(2)
+
+if c1.button("실행 (SSE)"):
     for event in start_process():
         pbar.progress(event['status'], event['msg'])
     pbar.success("Complete")
     res = get_result()
     st.write(res)
+
+if c2.button("실행 (WS)"):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    asyncio.get_event_loop().run_until_complete(start_process_ws())
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import StreamingResponse
 import json
 import pandas as pd
-import time
+import logging
+from typing import List
+
+
+logging.basicConfig(
+    format='[%(levelname)s] %(asctime)s (%(name)s) :: %(message)s',
+    level=logging.DEBUG,
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+)
 
 
 app = FastAPI()
 
 
+# 1. SSE
 class Job():
     def __init__(self):
         self._result = pd.DataFrame()
@@ -18,12 +27,16 @@ class Job():
         return self._result
 
     async def query(self):
-        await asyncio.sleep(3)
+        for i in range(10):
+            logging.debug(f"query {i}")
+            await asyncio.sleep(0.2)
         self._result = pd.DataFrame({"A": [1, 2, 3], "B": [10, 20, 30]})
 
     async def preprocess(self):
-        await asyncio.sleep(3)
-        self._result = pd.DataFrame({"A": [1, 2, 3, 4], "B": [10, 20, 30, 40]})
+        for i in range(10):
+            logging.debug(f"preprocess {i}")
+            await asyncio.sleep(0.2)
+        self._result = pd.concat([self.result, pd.DataFrame({"A": [4], "B": [40]})]).reset_index(drop=True)
 
     async def process(self):
         yield json.dumps({"status": 0.1, "msg": "query"}) + "\n"
@@ -40,12 +53,37 @@ job = Job()
 async def main():
     return {"msg": "Hello World"}
 
-
 @app.get("/process")
 async def start_process():
     return StreamingResponse(job.process(), media_type='application/json')
 
-
 @app.get("/result")
 async def return_result():
     return {"result": job.result.to_json()}
+
+
+# 2. WebSocket
+async def query_ws():
+    for i in range(10):
+        logging.debug(f"query {i}")
+        await asyncio.sleep(0.2)
+    return pd.DataFrame({"A": [1, 2, 3], "B": [10, 20, 30]})
+
+async def preprocess_ws(df):
+    for i in range(10):
+        logging.debug(f"preprocess {i}")
+        await asyncio.sleep(0.2)
+    return pd.concat([df, pd.DataFrame({"A": [4], "B": [40]})]).reset_index(drop=True)
+
+@app.websocket("/process")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        await websocket.send_json(json.dumps({"status": 0.1, "msg": "query"}))
+        df = await query_ws()
+        await websocket.send_json(json.dumps({"status": 0.5, "msg": "preprocess"}))
+        df = await preprocess_ws(df)
+        await websocket.send_json(json.dumps({"status": 1.0, "msg": "complete"}))
+        await websocket.send_json(json.dumps({"result": df.to_json()}))
+    except WebSocketDisconnect:
+        logging.info(f"Client {websocket} disconnected")
